fb7b9f23c9b139226cdaf1caa53e866a0834cfbe
Lola Raji <74083700+lolaraji@users.noreply.github.com>
2021-05-10 10:08:27 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,14 +1,4 @@
-    	  var firebaseConfig = {
-    apiKey: "AIzaSyBkRxy6zmSQb410qqfrPHJ_Vh72NfcMefM",
-    authDomain: "lnkshrt-gq.firebaseapp.com",
-    projectId: "lnkshrt-gq",
-    storageBucket: "lnkshrt-gq.appspot.com",
-    messagingSenderId: "416337524579",
-    appId: "1:416337524579:web:8c21b49c98ed2fdc278556",
-    measurementId: "G-H2VBCMDSSN"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// firebase config info
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -21,5 +11,5 @@ if (urlParams.has("u")) {
 		window.location.href = lnk;
 	});
 } else {
-	window.location.href = "https://link-shortener.lolaraji.repl.co";
+	window.location.href = ""; // go back to main page where generation occurs.
 }
